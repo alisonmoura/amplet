b7b6d4f08aa0ef3a8560bda31c10ccc4eb855432
Alison Moura <alison.oghino@gmail.com>
2016-10-01 18:01:58 -0400

    Segundo commit, criando JFrame

--- src/com/amplet/Launcher.java
+++ src/com/amplet/Launcher.java
@@ -1,9 +1,22 @@
 package com.amplet;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Launcher {
 
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Projeto Amplet");
+        frame.setSize(new Dimension(500,500));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        Box box = Box.createVerticalBox();
+        JButton btn1 = new JButton("Clique-me");
+        box.add(btn1);
+
+        frame.add(box, BorderLayout.CENTER);
+
+        frame.setVisible(true);
     }
 
 }
