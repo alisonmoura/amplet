3c9fa09612b2dccb8610daa967a2fb5985126a8f
Alison Moura <alison.oghino@gmail.com>
2016-10-02 00:02:55 -0400

    Criando botão azul, com text branco

--- src/com/amplet/Launcher.java
+++ src/com/amplet/Launcher.java
@@ -12,9 +12,29 @@ public class Launcher {
         frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.LINE_AXIS));
 
         JButton btn1 = new JButton("Clique-me");
-        frame.getContentPane().add(btn1);
 
+        frame.getContentPane().add(btn1);
+        setBotaoAzulSemBordas(btn1);
         frame.setVisible(true);
+    }
+
+    private static void setBotaoAzulSemBordas(JButton button){
+        button.setBackground(getCorAzul());
+        button.setForeground(getCorBranca());
+        button.setBorder(BorderFactory.createLineBorder(getCorAzul(),20));
+    }
+
+    private static void setBotaoAzul(JButton button){
+        button.setBackground(getCorAzul());
+        button.setForeground(getCorBranca());
+    }
+
+    private static Color getCorBranca(){
+        return new Color(255,255,255);
+    }
+
+    private static Color getCorAzul(){
+        return new Color(0,0,255);
     }
 
 }
